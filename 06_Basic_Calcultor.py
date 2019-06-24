35403349524df9adc0126b7c2c495cb77e4c0a90
KMary12 <marzena.krempec@gmail.com>
2019-06-24 15:49:33 +0200

    this is my first commit in pucharm

--- 06_Basic_Calcultor.py
+++ 06_Basic_Calcultor.py
@@ -1,7 +1,7 @@
 num1 = input("Please provide first number:")
 num2 = input("Please provide second number:")
-result = int(num1) + int(num2)
+#result2 = int(num1) + int(num2)
 print(result)
 # float option
-result3= float(num1) + float(num2)
-print(result3)
+result3 = float(num1) + float(num2)
+print(round(float(result3)))
